f04933c7da26001607681963c1df126146810eaa
Sangmin Jeon <wjstkdals409@naver.com>
2019-05-30 20:05:17 +0900

    frame_ver2. upload
    Ready for merge function

--- temp.py
+++ temp.py
@@ -24,6 +24,44 @@ def print_board(lt):
     for i in range(4):
         print(lt[0+4*i],lt[1+4*i],lt[2+4*i],lt[3+4*i])
 
+def key_in(lt):
+    print("Distinguish only W/A/S/D")
+    x = input()
+    while True:
+        if x == 'w':
+            break
+        elif x == 'a':
+            break
+        elif x == 's':
+            break
+        elif x == 'd':
+            break
+        else:
+            print("INCORRECT INPUT!")
+    return lt
+
+# 1-Dimension board(Original)
+# 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+
+# 2-Dimension board
+# 0  1  2  3
+# 4  5  6  7
+# 8  9  10 11
+# 12 13 14 15
+
+def merge(oneline):
+    for i in range(4):
+        
+    return one
+
+def mu(lt):
+    for i in range(4):
+        lt[i],lt[4+i],lt[8+i],lt[12+i] = merge([lt[i],lt[4+i],lt[8+i],lt[12+i]])
+    return lt
+
+def md(lt):
+    for i in range(4):
+        merge([lt[
 def main():
     print("Welcome To 2048")
     print("Push W/A/S/D to Play.")
@@ -31,3 +69,4 @@ def main():
     bd = board_add(bd)
     bd = board_add(bd)
     print_board(bd)
+    
